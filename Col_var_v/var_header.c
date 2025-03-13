3b010d5ff03569b64549d80d8ca9fff41ef18d4b
hetpatel <hetfaldu19@gmail.com>
2025-03-13 10:54:05 -0400

    Update cost_var_v.nb to output CForm of cost to text file. Create new var_v_cost file for eval_f

--- Col_var_v/var_header.c
+++ Col_var_v/var_header.c
@@ -27,29 +27,6 @@ struct MyUserData
 
 /* Callback Functions */
 
-/* ---------------bool eval_f---------------
- * Method to request the value of the objective function.
- */
-static bool eval_f(
-    ipindex n,   // Number of variables
-    ipnumber *x, // Variables array
-    bool new_x,
-    ipnumber *obj_value, // Function
-    UserDataPtr user_data)
-{
-    assert(n == 55);
-    (void)n;
-
-    (void)new_x;
-    (void)user_data;
-    *obj_value = x[0] *
-                 (pow(x[9], 2) + pow(x[1], 2) + (2 * pow(x[2], 2)) + (2 * pow(x[3], 2)) + (2 * pow(x[4], 2)) +
-                  (2 * pow(x[5], 2)) + (2 * pow(x[6], 2)) + (2 * pow(x[7], 2)) + (2 * pow(x[8], 2))) /
-                 (16 * Ra_); // Function to be minimized
-
-    return true;
-} // End of eval_f
-
 /* ---------------bool eval_g---------------
  * Method to request the constraint values.
  */
