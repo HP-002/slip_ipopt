b040de6570b1ecd56d77b904e6660fd4dd1981c6
hetpatel <hetfaldu19@gmail.com>
2025-04-05 12:18:58 -0400

    parser functionality completed

--- parser.py
+++ parser.py
@@ -10,29 +10,43 @@ def parse_file(filename):
 
 def parse_equation(equation):
     # Fix the indices for variable 'x'
-    pattern = r'\bx(\d{1,2})\b'
     def replace(match):
         num = int(match.group(1)) - 1  # Subtract 1
         return f'x[{num}]'
-    parsed_equation = re.sub(pattern, replace, equation)
+    
+    equation = re.sub(r'\bx(\d{1,2})\b', replace, equation)
 
     # Remove all instances of "Subscript()"
-    parsed_equation = re.sub(r'Subscript\(\s*([A-Za-z0-9_]+)\s*,\s*([A-Za-z0-9_]+)\s*\)', r'\1\2', parsed_equation)
+    equation = re.sub(r'Subscript\(\s*([A-Za-z0-9_]+)\s*,\s*([A-Za-z0-9_]+)\s*\)', r'\1\2', equation)
 
     # Replace all instances of "Power(x,y)" with "pow(x,y)"
-    parsed_equation = re.sub(r'\bPower\b', 'pow', parsed_equation)
+    equation = re.sub(r'\bPower\b', 'pow', equation)
 
     # Replace all instances of "Sin(x)" with "sin(x)"
-    parsed_equation = re.sub(r'\bSin\b', 'sin', parsed_equation)
+    equation = re.sub(r'\bSin\b', 'sin', equation)
 
     # Replace all instances of "Cos(x)" with "cos(x)"
-    parsed_equation = re.sub(r'\bCos\b', 'cos', parsed_equation)
-    
-    return parsed_equation
+    equation = re.sub(r'\bCos\b', 'cos', equation)
+
+    # Replace all constants to avoid variable name conflicts between IPOpt and equations
+    equation = re.sub(r'\bg\b', 'G_', equation)     # g -> G_
+    equation = re.sub(r'\bRa\b', 'Ra_', equation)   # Ra -> Ra_
+    equation = re.sub(r'\bR\b', 'R_', equation)     # R -> R_
+    equation = re.sub(r'\bJ\b', 'J_', equation)     # J -> J_
+    equation = re.sub(r'\bb\b', 'B_', equation)     # b -> B_
+    equation = re.sub(r'\bc\b', 'C_', equation)     # c -> C_
+    equation = re.sub(r'\bm\b', 'M_', equation)     # m -> M_
+    equation = re.sub(r'\bl0\b', 'L0_', equation)   # l0 -> L0_
+    equation = re.sub(r'\bLa\b', 'La_', equation)   # La -> La_
+    equation = re.sub(r'\bk0\b', 'K0_', equation)   # k0 -> K0_
+    equation = re.sub(r'\bkb\b', 'Kb_', equation)   # kb -> Kb_
+    equation = re.sub(r'\bkt\b', 'Kt_', equation)   # kt -> Kt_
+
+    return equation
 
 
 def main():
-    input_files = ['Equations/Tests/test_equations_1.txt', 'Equations/Tests/test_equations_2.txt', 'Equations/Tests/test_equations_3.txt']
+    input_files = ['Equations/Tests/test_equations_1.txt', 'Equations/Tests/test_equations_2.txt', 'Equations/Tests/test_equations_3.txt', 'Equations/Tests/test_equations_4.txt']
 
     for file in input_files:
         parse_file(file)
