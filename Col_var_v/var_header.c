bfd8603b54438ced0143fb4fc4bcdad9ccd08ed0
hetpatel <hetfaldu19@gmail.com>
2025-04-18 18:01:16 -0400

    Update Global variables. Add Constraints for Variable V

--- Col_var_v/var_header.c
+++ Col_var_v/var_header.c
@@ -6,17 +6,17 @@
 #include <math.h>
 
 __uint64_t Ra_ = 1;
-__uint64_t b_ = 2;
-__uint64_t m_ = 3;
-__uint64_t g_ = 4; // Gravity?
-__uint64_t l0_ = 5;
-__uint64_t k0_ = 6;
-__uint64_t c_ = 7;
+__uint64_t B_ = 2;
+__uint64_t M_ = 3;
+__uint64_t G_ = 9.8; // Gravity?
+__uint64_t L0_ = 5;
+__uint64_t K0_ = 6;
+__uint64_t C_ = 7;
 __uint8_t R_ = 8;
-__uint64_t kt_ = 9;
+__uint64_t Kt_ = 9;
 __uint64_t J_ = 10;
 __uint64_t La_ = 11;
-__uint64_t kb_ = 12;
+__uint64_t Kb_ = 12;
 
 /* MyUserData contains our Problem */
 struct MyUserData
